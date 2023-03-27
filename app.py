4544b318e56ac5d47c40aee71a8fabb053211683
Tahmazyan <120594449+Tahmazyan@users.noreply.github.com>
2023-03-27 13:34:56 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from cmath import e
 import telebot
 from config import keys, TOKEN
 from utils import ConvertionException, CryptoConverter
@@ -21,19 +22,23 @@ def values(message: telebot.types.Message):
         text = "/n".join((text, key, ))
     bot.reply_to(message, text)
 
-
 @bot.message_handler(content_types=["text", ])
 def convert(message: telebot.types.Message):
-    values = message.text.split(" ")
+    try:
+        values = message.text.split(" ")
 
-    if len(values) != 3:
-        raise ConvertionException("Слишком много параметров.")
-    
-    quote, base, amount = values
-    total_base = CryptoConverter.convert(quote, base, amount)
+        if len(values) != 3:
+            raise ConvertionException("Слишком много параметров.")
 
-    text = f"Цена {amount} {quote} в {base} - {total_base}"
-    bot.send_message(message.chat.id, text)
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+       bot.reply_to(message, f"Ошибка пользователя\n{e}")
+    except Exception as e:
+       bot.reply_to(message, f"Не удалось обработать команду\n{e}")
+    else:
+       text = f"Цена {amount} {quote} в {base} - {total_base}"
+       bot.send_message(message.chat.id, text)
 
 
 bot.polling()
